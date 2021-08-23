49112dc21a5f60365a74c28dd6a53dd034e9e2ae
setar <40661123+setar@users.noreply.github.com>
2021-08-23 10:58:56 +0300

    Update install.sh
    fix

--- install.sh
+++ install.sh
@@ -15,14 +15,17 @@ CHIA_HOME=`su - $USER -c 'echo $HOME'`
 
 echo "Chia user home: $CHIA_HOME"
 
-sudo cp -v root-scripts/chia_* /usr/local/sbin/
+sudo chown root:root
+sudo chmod 700 root-scripts/chia_*
+sudo cp -vf root-scripts/chia_* /usr/local/sbin/
 
-echo "hardstatus alwayslastline
+sudo echo "hardstatus alwayslastline
 hardstatus string '%{gk}[ %{G}%H %{g}][%{= kw}%-w%{= BW}%n %t%{-}%+w][%= %{=b kR}(%{W} %h%?(%u)%?%{=b kR} )%{= kw}%=][%{Y}%l%{g}]%{=b C}[ %d.%m.%Y %c:%s ]%{W}'
 defscrollback 10000
 "  > $CHIA_HOME/.screenrc
-chown $USER:$USER $CHIA_HOME/.screenrc
+sudo chown $USER:$USER $CHIA_HOME/.screenrc
 
-mkdir -p $CHIA_HOME/chia-scripts
-cp -Rv chia-scripts/* $CHIA_HOME/chia-scripts/
-chown -R $USER:$USER $CHIA_HOME/chia-scripts
+sudo mkdir -p $CHIA_HOME/chia-scripts
+sudo cp -Rv chia-scripts/* $CHIA_HOME/chia-scripts/
+sudo chown -R $USER:$USER $CHIA_HOME/chia-scripts
+sudo chmod 700 $CHIA_HOME/chia-scripts/*.sh
